332e0a95bbbd8c5740b087d4a78342683c4daaa7
Blink <blink@thoughtworks.com>
2017-05-21 19:08:27 +0800

    [yanpei] #4 create method MarkAsError to mark error log entry for exception.

--- src/Axe.Logging.Core/AxeLogEntryExtension.cs
+++ src/Axe.Logging.Core/AxeLogEntryExtension.cs
@@ -22,6 +22,11 @@ namespace Axe.Logging.Core
             return MarkLogEntryForException(exception, AxeLogLevel.Warn, data);
         }
 
+        public static T MarkAsError<T>(this T exception, object data) where T : Exception
+        {
+            return MarkLogEntryForException(exception, AxeLogLevel.Error, data);
+        }
+
         static T MarkLogEntryForException<T>(T exception, AxeLogLevel axeLogLevel, object data) where T : Exception
         {
             var logEntry = new LogEntryMark(DateTime.UtcNow, data, axeLogLevel);
--- test/Axe.Logging.Test/AxeLogEntryExtensionFacts.cs
+++ test/Axe.Logging.Test/AxeLogEntryExtensionFacts.cs
@@ -44,6 +44,17 @@ namespace Axe.Logging.Test
         }
 
         [Fact]
+        public void should_get_error_log_entry_from_exception_marked_as_error()
+        {
+            object logEntryData = CreateLogEntryData();
+            Exception exception = new Exception().MarkAsError(logEntryData);
+
+            LogEntry logEntryResult = exception.GetLogEntry().Single();
+
+            VerifyLogEntry(logEntryResult, logEntryData, AxeLogLevel.Error);
+        }
+
+        [Fact]
         public void should_throw_argumet_null_exception_when_mark_exception_given_exception_is_null()
         {
             Exception exception = null;
